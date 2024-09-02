4b96ca4b4814274e1091045e78466de1ad2461aa
DennisSalmazs <skrn.sdet@gmail.com>
2024-09-02 14:18:19 +0100

    role and user repositories

---
+++ src/main/java/com/teambridge/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.teambridge.repository;
+
+import com.teambridge.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByDescription(String description);
+
+}
