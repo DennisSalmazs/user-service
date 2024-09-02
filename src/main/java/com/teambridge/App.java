b261ef0b5ca70157b0be07169bfc9bfcba82baaa
DennisSalmazs <skrn.sdet@gmail.com>
2024-09-02 12:57:50 +0100

    project setup

--- src/main/java/com/teambridge/App.java
+++ src/main/java/com/teambridge/App.java
@@ -1,13 +1,13 @@
-package com.teambridge.userservice;
+package com.teambridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
